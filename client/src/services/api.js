4a9f8fd62f72dd2c88c3c5190f57bc0b1e499db0
jbkelli <444mwangialvinm@gmail.com>
2025-11-17 12:34:00 +0300

    new skillswap update

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
-
-const API_URL = 'http://localhost:5000/api';
+import { API_BASE_URL } from '../config/api';
 
 // Create axios instance with default config
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json'
   }
